e07d67d4f616bffd23b0102277bedb7c3c8e1fd9
ghoste9624 <jackspade44@outlook.com>
2025-04-15 07:00:59 -0400

    Update anon.py

--- anon.py
+++ anon.py
@@ -6,7 +6,7 @@ git clone https://github.com/ghoste9624/anonymous-motd /data/data/com.termux/fil
 echo "/data/data/com.termux/files/usr/etc/motd/init.sh" >> /data/data/com.termux/files/usr/etc/profile # or .zprofile if using zsh
 cd /data/data/com.termux/files/usr/etc/motd/ && chmod +x init.sh && cd $HOME
 
-echo "PS1='\e[3m\[\033[1;30m\]\D{%a-%b-%d-%Y} \e[38;5;214m\@
+echo "PS1='\e[3m\[\033[1;30m\]\D{%a-%b-%d-%Y}( )\e[38;5;214m\@
 \[\033[1;91m\]\w
  \[\033[0;34m\][\[\033[0;36m\]\#\[\033[0;34m\]]\[\033[1;92m\] '" >> ~/.bashrc
 source ~/.bashrc
